0c59afa44f39af73f23154e377970dd9148f4207
chrisZac <zachiotis.sc@gmail.com>
2017-02-02 15:16:36 +0100

    Folder update

--- prev_workshop/obj_zoul/assert.d
+++
@@ -1,2 +0,0 @@
-obj_zoul/assert.o: ../contiki/core/lib/assert.c
-../contiki/core/lib/assert.c :
--- prev_workshop/obj_zoul/list.d
+++
@@ -1,2 +0,0 @@
-obj_zoul/list.o: ../contiki/core/lib/list.c ../contiki/core/./lib/list.h
-../contiki/core/lib/list.c ../contiki/core/./lib/list.h :
